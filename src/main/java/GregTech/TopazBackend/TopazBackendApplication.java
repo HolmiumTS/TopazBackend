0d3bb8712c00e6c13fa14cfa35ac07ba6b5f0195
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-11 10:54:24 +0800

    init

---
+++ src/main/java/GregTech/TopazBackend/TopazBackendApplication.java
@@ -0,0 +1,13 @@
+package GregTech.TopazBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopazBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopazBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GregTech/TopazBackend/TopazBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package GregTech.TopazBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopazBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
