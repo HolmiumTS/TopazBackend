59beba237e0037400c4c380b7ce56d044e6318a9
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-11 15:20:59 +0800

    add Register

--- src/main/java/GregTech/TopazBackend/response/Register.java
+++ src/main/java/GregTech/TopazBackend/response/Register.java
@@ -7,19 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class Register {
-    @RequestMapping(value = "/register", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "/register",
+            method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public Map<String, Object> response(@RequestBody Map<String, Object> body) {
         User user = new User();
         user.setEmail((String) body.get("email"));
         user.setName((String) body.get("username"));
         user.setTel((String) body.get("tel"));
-        int id;
-//        if(Users)
-        return null;
+        int id = Users.addUser(user);
+        Map<String, Object> res = new HashMap<>();
+        res.put("result", id == -1);
+        res.put("id", id);
+        return res;
     }
 
 }
