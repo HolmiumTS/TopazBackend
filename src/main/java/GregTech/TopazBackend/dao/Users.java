04f243f2be2ac766fba13c01b5b4c01cce5e5f6a
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-11 15:05:23 +0800

    add interface for user

---
+++ src/main/java/GregTech/TopazBackend/dao/Users.java
@@ -0,0 +1,46 @@
+package GregTech.TopazBackend.dao;
+
+import GregTech.TopazBackend.metadata.User;
+
+public abstract class Users {
+    /**
+     * @param id user's id
+     * @return null if no such user
+     */
+    public User getById(int id) {
+        return null;
+    }
+
+    /**
+     * @param email user's email
+     * @return null if no such email
+     */
+    public User getByEmail(String email) {
+        return null;
+    }
+
+    /**
+     * @param tel user's tel
+     * @return null if no such email
+     */
+    public User getByTel(String tel) {
+        return null;
+    }
+
+    /**
+     * @param user user with new info
+     * @return false if email or tel is duplicate, otherwise true
+     */
+    public boolean updateUser(User user) {
+        return false;
+    }
+
+    /**
+     * @param user user's info, notice that 'id' is set to -1
+     * @return user's id, -1 if failed
+     */
+    public int addUser(User user) {
+        return -1;
+    }
+
+}
