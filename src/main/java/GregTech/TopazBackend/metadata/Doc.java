e726f1c1d779c3af7085c50a22f008dc69d31efb
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-11 14:40:04 +0800

    add Team and Doc

--- src/main/java/GregTech/TopazBackend/metadata/Doc.java
+++ src/main/java/GregTech/TopazBackend/metadata/Doc.java
@@ -1,4 +1,103 @@
 package GregTech.TopazBackend.metadata;
 
 public class Doc {
+    int did;
+    String name;
+    int owner;
+    int team;
+    boolean view;
+    boolean edit;
+    int create;
+    int update;
+    int count;
+    String content;
+    boolean isDel;
+
+    public int getDid() {
+        return did;
+    }
+
+    public void setDid(int did) {
+        this.did = did;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public int getTeam() {
+        return team;
+    }
+
+    public void setTeam(int team) {
+        this.team = team;
+    }
+
+    public boolean isView() {
+        return view;
+    }
+
+    public void setView(boolean view) {
+        this.view = view;
+    }
+
+    public boolean isEdit() {
+        return edit;
+    }
+
+    public void setEdit(boolean edit) {
+        this.edit = edit;
+    }
+
+    public int getCreate() {
+        return create;
+    }
+
+    public void setCreate(int create) {
+        this.create = create;
+    }
+
+    public int getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(int update) {
+        this.update = update;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isDel() {
+        return isDel;
+    }
+
+    public void setDel(boolean del) {
+        isDel = del;
+    }
 }
--- src/main/java/GregTech/TopazBackend/metadata/Team.java
+++ src/main/java/GregTech/TopazBackend/metadata/Team.java
@@ -4,4 +4,37 @@ public class Team {
     int tid;
     String name;
     int owner;
+    String info;
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
