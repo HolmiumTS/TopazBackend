6a735c0b4b0689e62aad011af81cf6d2e2588970
generaluseai <general_use_ai@163.com>
2020-08-11 22:40:36 +0800

    finished functions of getting Teams

--- src/main/java/GregTech/TopazBackend/dao/Teams.java
+++ src/main/java/GregTech/TopazBackend/dao/Teams.java
@@ -1,27 +1,53 @@
 package GregTech.TopazBackend.dao;
 
 import GregTech.TopazBackend.metadata.Team;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Teams {
+    @Resource
+    private static JdbcTemplate jdbc = new JdbcTemplate();
+
+    private static class TeamsMapper implements RowMapper<Team>{
+
+        @Override
+        public Team mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Team team=new Team();
+            team.setTid(rs.getInt("tid"));
+            team.setInfo(rs.getString("info"));
+            team.setName(rs.getString("name"));
+            team.setOwner(rs.getInt("owner"));
+            return team;
+        }
+    }
+
     /**
      * @param id user's id
      * @return a list of all teams that include this user, return an empty list if none
      */
     public static List<Team> getTeamsById(int id) {
-        //todo
+            String sql ="";
+            List<Team> teams = jdbc.query(sql,new TeamsMapper());
         return new ArrayList<>();
     }
-
-
     /**
      * @param tid team id
      * @return null if no such team
      */
     public static Team getTeamByTid(int tid) {
-        //todo
+    String sql="select * from Team T where tid=?";
+    try {
+        Team team =jdbc.queryForObject(sql, new TeamsMapper(),tid);
+        return team;
+    }catch (EmptyResultDataAccessException e){
         return null;
+    }
     }
 }
