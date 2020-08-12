6c11c9fffc9ec82fd3d4a8eb37fe575d34e73e05
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-12 11:35:51 +0800

    upd Teams

--- src/main/java/GregTech/TopazBackend/dao/Teams.java
+++ src/main/java/GregTech/TopazBackend/dao/Teams.java
@@ -15,11 +15,11 @@ public abstract class Teams {
     @Resource
     private static JdbcTemplate jdbc = new JdbcTemplate();
 
-    private static class TeamsMapper implements RowMapper<Team>{
+    private static class TeamsMapper implements RowMapper<Team> {
 
         @Override
         public Team mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Team team=new Team();
+            Team team = new Team();
             team.setTid(rs.getInt("tid"));
             team.setInfo(rs.getString("info"));
             team.setName(rs.getString("name"));
@@ -34,21 +34,34 @@ public abstract class Teams {
      */
     public static List<Team> getTeamsById(int id) {
         //todo
-            String sql ="";
-            List<Team> teams = jdbc.query(sql,new TeamsMapper());
+        String sql = "";
+        List<Team> teams = jdbc.query(sql, new TeamsMapper());
         return new ArrayList<>();
     }
+
     /**
      * @param tid team id
      * @return null if no such team
      */
     public static Team getTeamByTid(int tid) {
-    String sql="select * from Team T where tid=?";
-    try {
-        Team team =jdbc.queryForObject(sql, new TeamsMapper(),tid);
-        return team;
-    }catch (EmptyResultDataAccessException e){
-        return null;
+        String sql = "select * from Team T where tid=?";
+        try {
+            Team team = jdbc.queryForObject(sql, new TeamsMapper(), tid);
+            return team;
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
+
+    /**
+     * Set a user in a team to be (or not to be) an admin
+     *
+     * @param tid     team id
+     * @param id      user id
+     * @param isAdmin true (add an admin), false (del an admin)
+     * @return false if the admin is the owner
+     */
+    public static boolean setAdmin(int tid, int id, boolean isAdmin) {
+        return true;
     }
 }
