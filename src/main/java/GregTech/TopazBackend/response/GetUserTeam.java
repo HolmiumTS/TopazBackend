3e351ac834b38b9b153dfb2b7b47f2aacec141ca
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-12 10:12:34 +0800

    add GetUserTeam

--- src/main/java/GregTech/TopazBackend/response/GetUserTeam.java
+++ src/main/java/GregTech/TopazBackend/response/GetUserTeam.java
@@ -1,6 +1,8 @@
 package GregTech.TopazBackend.response;
 
+import GregTech.TopazBackend.dao.Teams;
 import GregTech.TopazBackend.dao.Users;
+import GregTech.TopazBackend.metadata.Team;
 import GregTech.TopazBackend.metadata.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class GetUserTeam {
@@ -18,9 +22,17 @@ public class GetUserTeam {
 
     @RequestMapping(value = "/GetUserTeam",
             method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public Map<String, Object> response(@RequestBody Map<String, Object> body) {
-        //todo
-        return null;
+    public List<Map<String, Object>> response(@RequestBody Map<String, Object> body) {
+        int id = Integer.parseInt((String) body.get("id"));
+        List<Team> result = Teams.getTeamsById(id);
+        return result.stream().map(this::collectData).collect(Collectors.toList());
+    }
+
+    private Map<String, Object> collectData(Team team) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", team.getTid());
+        map.put("name", team.getName());
+        return map;
     }
 
 }
