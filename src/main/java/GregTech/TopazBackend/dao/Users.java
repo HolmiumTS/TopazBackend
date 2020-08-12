a495b745ee688c552d295f9868df116fe3349180
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-12 11:15:29 +0800

    upd Users

--- src/main/java/GregTech/TopazBackend/dao/Users.java
+++ src/main/java/GregTech/TopazBackend/dao/Users.java
@@ -13,6 +13,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Users {
     @Resource
@@ -21,7 +23,7 @@ public abstract class Users {
     /**
      * @return return RowMapper<User> as the second argument of queryForObject
      */
-    private static class UserMapper implements RowMapper<User>{
+    private static class UserMapper implements RowMapper<User> {
 
         @Override
         public User mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -45,9 +47,9 @@ public abstract class Users {
     public static User getById(int id) {
         try {
             String sql = "select  * from User U where U.id=?";
-            User user = jdbc.queryForObject(sql,new UserMapper(), id);
+            User user = jdbc.queryForObject(sql, new UserMapper(), id);
             return user;
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
 
@@ -62,7 +64,7 @@ public abstract class Users {
             String sql = "select  * from User U where U.email=?";
             User user = jdbc.queryForObject(sql, new UserMapper(), email);
             return user;
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
 
@@ -73,11 +75,11 @@ public abstract class Users {
      * @return null if no such email
      */
     public static User getByTel(String tel) {
-        try{
+        try {
             String sql = "select  * from User U where U.tel=?";
             User user = jdbc.queryForObject(sql, new UserMapper(), tel);
             return user;
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
 
@@ -90,13 +92,13 @@ public abstract class Users {
     public static boolean updateUser(User user) {
         String sql = "update User u set u.id=?,u.name=?,u.password=?,u.email=?" +
                 ",u.latestDoc=?,u.tel=?,u.avatar=? where u.id=?";
-            int i = jdbc.update(sql, user.getId(), user.getName(), user.getPassword()
-                    , user.getEmail(), user.getLatestDoc(), user.getTel(), user.getAvatar(), user.getId());
-            if (i > 0) {
-                return true;
-            } else {
-                return false;
-            }
+        int i = jdbc.update(sql, user.getId(), user.getName(), user.getPassword()
+                , user.getEmail(), user.getLatestDoc(), user.getTel(), user.getAvatar(), user.getId());
+        if (i > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -105,30 +107,51 @@ public abstract class Users {
      */
     public static int addUser(User user) {
         String sql = "insert  into User(name,password,email,latestDoc,tel,avatar) values(?,?,?,?,?,?) ";
-            KeyHolder keyHolder=new GeneratedKeyHolder();
-            int i =jdbc.update(new PreparedStatementCreator() {
-                @Override
-                public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                    PreparedStatement ps =con.prepareStatement(sql,new String[]{"id"});
-                    ps.setString(1,user.getName());
-                    ps.setString(2,user.getPassword());
-                    ps.setString(3,user.getEmail());
-                    ps.setString(4,I2S(user.getLatestDoc()));
-                    ps.setString(5,user.getTel());
-                    ps.setString(6,user.getAvatar());
-                    return ps;
-                    }
-            },keyHolder);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int i = jdbc.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+                ps.setString(1, user.getName());
+                ps.setString(2, user.getPassword());
+                ps.setString(3, user.getEmail());
+                ps.setString(4, I2S(user.getLatestDoc()));
+                ps.setString(5, user.getTel());
+                ps.setString(6, user.getAvatar());
+                return ps;
+            }
+        }, keyHolder);
         return keyHolder.getKey().intValue();
     }
 
-    private static String I2S(int[] docs){
-        String[] bb=new String[docs.length];
-        for (int i=0;i<docs.length;i++){
-            bb[i]=Integer.toString(docs[i]);
+    private static String I2S(int[] docs) {
+        String[] bb = new String[docs.length];
+        for (int i = 0; i < docs.length; i++) {
+            bb[i] = Integer.toString(docs[i]);
         }
-        String s="";
-        s.join(";",docs.toString());
+        String s = "";
+        s.join(";", docs.toString());
         return s;
+    }
+
+    /**
+     * get all user who is not an admin in a team
+     *
+     * @param tid id of the team
+     * @return an empty list if no such user exists
+     */
+    public static List<User> getNormalUserByTid(int tid) {
+        //todo
+        return new ArrayList<>();
+    }
+
+    /**
+     * get all admin in a team
+     * @param tid id of the team
+     * @return a list, (of course there is at least one admin in a team that actually exists)
+     */
+    public static List<User> getAdminUserByTid(int tid) {
+        //todo
+        return new ArrayList<>();
     }
 }
