d8e5130fa35b6a98a1d5645e7cc653be89a7f279
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-08-13 14:20:53 +0800

    add DissolveTeam

---
+++ src/main/java/GregTech/TopazBackend/response/DissolveTeam.java
@@ -0,0 +1,40 @@
+package GregTech.TopazBackend.response;
+
+import GregTech.TopazBackend.dao.Teams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DissolveTeam {
+    private static final Logger log = LoggerFactory.getLogger(CreateTeam.class);
+
+    private final Teams teamDao;
+
+    @Autowired
+    public DissolveTeam(Teams teamDao) {
+        this.teamDao = teamDao;
+    }
+
+    @RequestMapping(value = "/DissolveTeam",
+            method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public Map<String, Object> response(@RequestBody Map<String, Object> body) {
+        Map<String, Object> res = new HashMap<>();
+        int tid = Integer.parseInt((String) body.get("teamId"));
+        boolean r = teamDao.delTeam(tid);
+        if (r) {
+            log.trace("Delete successfully.");
+        } else {
+            log.warn("Delete failed: Unknown error.");
+        }
+        res.put("result", r);
+        return res;
+    }
+}
